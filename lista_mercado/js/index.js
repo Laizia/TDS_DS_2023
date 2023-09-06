787c65dd8892796664f1bd67bdd7e2c11a4dfbb5
laizia <laiziadasilva@gmail.com>
2023-09-06 17:07:52 -0300

    inplementação de recursos de interação em tela

--- lista_mercado/js/index.js
+++ lista_mercado/js/index.js
@@ -1,22 +1,64 @@
 const cardContent = window.document.getElementsByClassName("card-content")[0];
 var countIndex = 0;
 
-function addItem(){
-    const item = window.document.getElementById(countIndex);
-    console.log(countIndex, item);
-    appendComponent();
+function deleteComponent(index){
+   const itemDelete = window.document.getElementById(index)
+   itemDelete.outerHTML = "";
+
 }
 
-console.log(cardContent);
-
-function appendComponent(){
-    var li = window.document.createElement("li");
-    li.setAttribute("id", 1)
-    li.setAttribute("class", "card-content-item");
+function onChecked(index){
+    const itemChecked = window.document.getElementById(index)
+    const itemRad = itemChecked.getElementsByClassName("item-rad")[0];
+    const span = itemRad.getElementsByTagName("span")[0];
+    const contentInput = itemChecked.getElementsByClassName("card-content-input")[0];
     
+    const isExistElemnt = itemChecked.getElementsByClassName("item-rad-checked"); 
+    
+    const input = itemChecked.getElementsByTagName("input")[0];
+
+    //checked
+    
+   
+    if(isExistElemnt.length <= 0) {
+        if (input.value.trim() !== "") {
+        itemRad.setAttribute("class", "item-rad item-rad-checked");
+        contentInput.setAttribute("class", "card-content-input card-content-input-checked");
+        span.innerHTML = "check_circle";
+        }
+    }
+    else{
+        itemRad.setAttribute("class", "item-rad");
+        contentInput.setAttribute("class", "card-content-input");
+        span.innerHTML = "radio_button_unchecked";
+    }
+   
+}
+
+function addItem() {
+
+    const item = document.getElementById(countIndex - 1);
+    console.log(item);
+    if (item) {
+        const input = item.getElementsByTagName("input")[0];
+        if (input.value.trim() !== "") {
+            appendComponent();
+        }
+    }
+    else {
+        appendComponent();
+    }
+}
+
+function appendComponent() {
+    var li = window.document.createElement("li");
+    li.setAttribute("id", countIndex++)
+    li.setAttribute("class", "card-content-item");
+
     //CONSTROI DIV-DEL
     var btnClose = window.document.createElement("div");
     btnClose.setAttribute("class", "item-del");
+    btnClose.setAttribute("onClick", "deleteComponent(" + (countIndex - 1) + ")");
 
     li.appendChild(btnClose);
 
@@ -35,12 +77,19 @@ function appendComponent(){
 
     var input = window.document.createElement("input");
 
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+           addItem();        }
+    });
+
     cardContentInput.appendChild(input);
     itemDescription.appendChild(cardContentInput);
     li.appendChild(itemDescription);
 
     var itemRad = window.document.createElement("div");
     itemRad.setAttribute("class", "item-rad");
+
+    itemRad.setAttribute("onClick", "onChecked(" + (countIndex-1) +")" );
 
     var spanChecked = window.document.createElement("span");
     spanChecked.setAttribute("class", "material-symbols-outlined");
@@ -51,7 +100,7 @@ function appendComponent(){
     li.appendChild(itemRad);
 
     cardContent.appendChild(li);
-    
+
     input.focus();
 
 }
