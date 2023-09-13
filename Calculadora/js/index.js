b91e9db91395c852195f8ebbdbe7f8095073e619
laizia <laiziadasilva@gmail.com>
2023-09-13 17:11:40 -0300

    atividade de Criação de calculadora dia 13/09/2023

---
+++ Calculadora/js/index.js
@@ -0,0 +1,11 @@
+function insert(num){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+
+
+}
+
+function divisao{
+    const resultado = Number(fist_num.value);
+    if second_num
+}
